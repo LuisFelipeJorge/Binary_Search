0ca4f77f187bef2e094f8921cff1a143cb16ab4b
LuisFelipeJorge <luisfelipe.jorge123@gmail.com>
2020-01-30 22:24:22 -0300

    Analysing the Recursive Approach

--- binary_SR.c
+++ binary_SR.c
@@ -13,26 +13,51 @@ long int binarySearchR(long int x, long int low, long int upp, long int a[]); //
 /////////////////////////////////////////////////////
 
 int main(int argc, char const *argv[]) {
-  long int n = 100;
-  long int *a = (long int *)malloc(n*sizeof(long int ));
-  if (a == NULL) {
-    printf("Memory could not be allocated" );
-    exit(EXIT_FAILURE); // in this case, we have a problem in the process of allocating memory for the array
-  }else{
-    random_array(n,a);
-    print_array(n,a);
-    qsort(a,n,sizeof(long int),cmpfunc);// we will use the C standard function for quick sort to sort the input array, and then allow us to test the binary search algorithm
-    print_array(n,a);
-    long int b;
-    b = binarySearchR(95,0,n,a);
-    if (b == -1){
-      printf("The element doesen't belong to the array !!\n");
-    }else {
-      printf("Value Found\n");
-      printf("b = %ld\n", b );
+  int k; // represents the power of 10, and this will be the size of the array
+  long int n;// n --> the size of the array
+  clock_t t_start1,t_start2,t_end1,t_end2; // variables to measure the time passed to sort the array
+  double timeUsed1,timeUsed2;
+  for (k = 2; k <= 6; k++) {
+
+    n = 1;
+    int i = k;
+    while (i >= 0){
+      n = n*10;
+      i--;
     }
 
-    free(a);
+    long int *a = (long int *)malloc(n*sizeof(long int ));
+    if (a == NULL) {
+      printf("Memory could not be allocated" );
+      exit(EXIT_FAILURE); // in this case, we have a problem in the process of allocating memory for the array
+    }else{
+      random_array(n,a);
+      //print_array(n,a);
+      printf("K = %d\n", k );
+
+      t_start1 = clock();
+      qsort(a,n,sizeof(long int),cmpfunc);// we will use the C standard function for quick sort to sort the input array, and then allow us to test the binary search algorithm
+      t_end1 = clock();
+      //print_array(n,a);
+
+      long int b;
+      t_start2 = clock();
+      b = binarySearchR(95,0,n,a);
+      t_end2 = clock();
+      if (b == -1){
+        printf("The element doesen't belong to the array !!\n");
+      }else {
+        printf("Value Found\n");
+        printf("b = %ld\n", b );
+      }
+      timeUsed1 = ((double)(t_end1 - t_start1))/CLOCKS_PER_SEC;
+      printf("Time passed for sorting : %f \n", timeUsed1);
+      timeUsed2 = ((double)(t_end2 - t_start2))/CLOCKS_PER_SEC;
+      printf("Time passed for searching : %f \n", timeUsed2);
+      printf("Total time : %f \n",(timeUsed1 + timeUsed2) );
+
+      free(a);
+    }
   }
   return 0;
 }
